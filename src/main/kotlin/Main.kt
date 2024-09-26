6ce1d9b356cf0c289c2fee9ac434ff617a5177d1
guofan <fan.guo@telecwin.com>
2024-09-26 15:29:17 +0800

    feat:add splash stage

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,11 +8,16 @@ import javafx.geometry.Pos
 import javafx.scene.Scene
 import javafx.scene.control.Button
 import javafx.scene.control.Label
+import javafx.scene.image.Image
+import javafx.scene.image.ImageView
 import javafx.scene.layout.VBox
 import javafx.stage.Stage
+import javafx.stage.StageStyle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.ConfigurableApplicationContext
@@ -26,8 +31,6 @@ class Main : Application() {
 
         @JvmStatic
         fun main(args: Array<String>) {
-            appContext = runApplication<Main>(*args)
-            logger.info("初始化SpringBoot完成！")
             launch(Main::class.java, *args)
         }
     }
@@ -36,31 +39,62 @@ class Main : Application() {
         var value: Int = 0
         stage!!
         stage.title = "Hello JavaFX"
-        val vBox = VBox().apply {
+        val image = Image(javaClass.getResourceAsStream("/images/splash.png"))
+        val splashView = ImageView(image)
+        val vBox = VBox()
+        vBox.prefWidth = image.width
+        vBox.prefHeight = image.height
+        vBox.apply {
             alignment = Pos.CENTER
-            children.add(Label("hello word!").apply {
-                textProperty().bindBidirectional(textProp)
-            })
-            children.add(Button().apply {
-                text = "点击"
-                setOnAction {
-                    logger.info("按钮被单击！")
-                    value++
-                    textProp.set("当前值$value")
-                }
-            })
+            children.add(splashView)
         }
-        val scene = Scene(vBox, 400.0, 300.0) // 创建场景
+        val splashScene = Scene(vBox)
+        val splashStage = Stage()
+        splashStage.scene = splashScene
+        splashStage.initStyle(StageStyle.UNDECORATED)
+        splashStage.show()
+        val scope = CoroutineScope(Dispatchers.Default)
+        val uiScope = CoroutineScope(Dispatchers.Main)
+        scope.launch {
+            println("当前线程：${Thread.currentThread().name}")
+            initSpringBoot()
+            uiScope.launch {
+                println("当前线程：${Thread.currentThread().name}")
+                vBox.apply {
+                    children.clear()
+                    alignment = Pos.CENTER
+                    children.add(Label("hello word!").apply {
+                        textProperty().bindBidirectional(textProp)
+                    })
+                    children.add(Button().apply {
+                        text = "点击"
+                        setOnAction {
+                            logger.info("按钮被单击！")
+                            value++
+                            textProp.set("当前值$value")
+                        }
+                    })
+                }
+                splashStage.close()
+                stage.apply {
+                    show() // 显示窗口
+                    setOnCloseRequest {
+                        appContext.close()
+                        logger.info("关闭SpringBoot")
+                    }
+                }
+            }
+        }
+    }
 
+    /**
+     * 初始化SpringBoot
+     */
+    private fun initSpringBoot() {
+        appContext = runApplication<Main>()
         val service: MyService = appContext.getBean(MyService::class.java)
         println("${service.greet()}")
-
-        stage.scene = scene // 设置场景
-        stage.show() // 显示窗口
-        stage.setOnCloseRequest {
-            appContext.close()
-            logger.info("关闭SpringBoot")
-        }
+        logger.info("初始化SpringBoot完成！")
     }
 
 }
